cee478a3f6198a9ccf5ea1a998e8fe1e11166323
joer9514 <1630@holbertonschool.com>
2020-03-14 19:26:40 +0000

    mandatory file function

---
+++ _printfMa.c
@@ -0,0 +1,86 @@
+#include "holberton.h"
+/**
+  * _cprintf - prints char format
+  * @format: type of format
+  * Return: length
+  */
+int _cprintf(va_list format)
+{
+	char store;
+
+	store = va_arg(format, int);
+
+	write(1, &store, 1);
+
+	return (1);
+}
+/**
+  * _sprintf - print string format
+  * @format: string
+  * Return: length
+  */
+int _sprintf(va_list format)
+{
+	int i;
+	char *ptr;
+
+	ptr = va_arg(format, char *);
+
+	for (i = 0; ptr[i]; i++)
+	{
+	write(1, &ptr, 1);
+		ptr++;
+	}
+	return (i);
+}
+/**
+  * _iprintf - print integer format
+  * @format: integer
+  * Return: int
+  */
+int _iprintf(va_list format)
+{
+	char negativ;
+	int i, count, num;
+
+	i = va_arg(format, int);
+	num = i;
+	count = 0;
+	negativ = '-';
+
+	if (i < 0)
+	{
+		count++;
+		write(1, &negativ, 1);
+		i = -i;
+	}
+	while (num != 0)
+	{
+		num = num / 10;
+		count += 1;
+	}
+	_iprintf((unsigned int)i);
+	return (count);
+}
+/**
+ * _printf - function of option struct
+ * @format: parameter pointer
+ * Return: option of struct
+ */
+int _printf(const char *format, ...)
+{
+	_format option[] = {
+		{ "c", _cprintf},
+		{ "s", _sprintf},
+		{ "d", _dprintf},
+		{ "%", _Prprintf},
+		{ "i", _iprintf},
+		{ "b", _bprintf},
+		{ "r", _rprintf},
+		{ "R", _Rprintf},
+		{ "u", _uprintf},
+		{ "o", _oprintf},
+		{ "x", _xprintf},
+		{ "X", _Xprintf},
+	};
+}
