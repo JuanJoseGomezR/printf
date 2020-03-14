f3aeaeb3c3b7143f13558c3ec838b0aa3ef034b0
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-14 16:40:20 +0000

    commit

--- _printf.c
+++ _printf.c
@@ -3,10 +3,28 @@
 /**
   * _cprintf - prints char format
   * @format: type of format
-  * Return: int
+  * Return: length
   */
-int _cprintf(va_list format);
+int _cprintf(va_list format)
 {
-	return(write(1, &c, 1);
+	return(write(1, &c, 1));
 }
+/**
+  * _sprintf - print string format
+  * @format: string
+  * Return: length
+  */
+int _sprintf(va_list format)
+{
+	int i;
+	char *ptr;
 
+	ptr = va_arg(args, char *);
+
+	for (i = 0; s[i]; i++)
+	{
+	write(1, s, 1);
+		s++;
+	}
+	return (i);
+}
