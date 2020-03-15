95bdc1ff1ba8f32d07305c2e952570aca16ac794
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-15 23:18:26 +0000

    Updated printf

--- _printf.c
+++ _printf.c
@@ -10,40 +10,36 @@ int _printf(const char *format, ...)
 	_format option[] = {
 		{ "c", _cprintf},
 		{ "s", _sprintf},
-		{ "d", _dprintf},
+		{ "d", _iprintf},
 		{ "%", _Prprintf},
 		{ "i", _iprintf},
 		{ "b", _bprintf},
-		{ "r", _rprintf},
-		{ "R", _Rprintf},
-		{ "u", _uprintf},
-		{ "x", _xprintf},
-		{NULL, NULL},
+		{'\0', NULL},
 	};
-va_list valist;
-int i, j, x;
+	va_list valist;
+	int i, j, x;
 
-j = 0;
-va_start(valist, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (0);
-for (i = 0; format[i]; i++)
-{
-	for (x = 0; option[x].type != '\0'; x++)
+	j = 0;
+	va_start(valist, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
+	for (i = 0; format[i]; i++)
 	{
-		if (format[i] == '%')
+		for (x = 0; option[x].op != '\0'; x++)
 		{
-			if (format[i + 1] == option[x].type)
+			if (format[i] == '%')
 			{
-				i = i + 2;
-				j += option[x].f(valist);
+				if (format[i + 1] == option[x].op)
+				{
+					i = i + 2;
+					j += option[x].f(valist);
+				}
+				else if (format[i + 1] == '%')
+					i = i + 1;
 			}
-			else if (format[i + 1] == '%')
-				i = i + 1;
 		}
+		j = j + _putchar(format[i]);
 	}
-	j += _cprintf(format[i]);
-}
-va_end(valist);
-return (j);
+	va_end(valist);
+	return (j);
 }
