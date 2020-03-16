0cfb4001769d8c20341c7d34b741e5f6a73b0ae1
joer9514 <1630@holbertonschool.com>
2020-03-16 15:55:53 +0000

    updated library

--- holberton.h
+++ holberton.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdarg.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-int _putchar(char c);
+int _write(char c);
 int _strlen(char *s);
 int _printf(const char *format, ...);
 int _cprintf(va_list format);
